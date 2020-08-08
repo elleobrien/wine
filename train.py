2786151b7cd7d3d463aa6917f6af8eef796169ad
Sayak Paul <spsayakpaul@gmail.com>
2020-08-08 20:09:28 +0530

    adding wandb support

--- train.py
+++ train.py
@@ -1,11 +1,25 @@
+# Imports
 import pandas as pd 
+import argparse
+import wandb
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+
 # Set random seed
 seed = 42
+
+# Construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-k", "--key", required=True,
+	help="wandb API key")
+args = vars(ap.parse_args())
+
+# Set wandb up
+wandb.login(key=args["key"])
+wandb.init(project="wanbd-github-actions", entity="sayakpaul")
 
 ################################
 ########## DATA PREP ###########
@@ -31,55 +45,5 @@ train_score = regr.score(X_train, y_train) * 100
 # Report test set score
 test_score = regr.score(X_test, y_test) * 100
 
-# Write scores to a file
-with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
-
-
-##########################################
-##### PLOT FEATURE IMPORTANCE ############
-##########################################
-# Calculate feature importance in random forest
-importances = regr.feature_importances_
-labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
-feature_df = feature_df.sort_values(by='importance', ascending=False,)
-
-# image formatting
-axis_fs = 18 #fontsize
-title_fs = 22 #fontsize
-sns.set(style="whitegrid")
-
-ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs) 
-ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
-ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
-
-plt.tight_layout()
-plt.savefig("feature_importance.png",dpi=120) 
-plt.close()
-
-
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
-
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
-
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
-
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
-
-plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
-
+# Log plots for the regressor
+wandb.sklearn.plot_regressor(regr, X_train, X_test, y_train, y_test)
