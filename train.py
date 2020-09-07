2dbcfa63a8fbbbbfb7ca74410517867f991e9992
Sayak Paul <spsayakpaul@gmail.com>
2020-09-07 16:06:12 +0530

    adding

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ subprocess.Popen(["git", "config", "--global", "user.name", "github-actions[bot]
 subprocess.Popen(["git", "config", "--global", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"])
 
 print("Push to remote...")
-subprocess.Popen(["git", "rm", "wandb"])
+subprocess.Popen(["rm", "-rf", "wandb"])
 subprocess.Popen(["git", "add", "-A"])
 subprocess.Popen(["git", "commit", "-m", run_url])
 subprocess.Popen(["git", "push"])
