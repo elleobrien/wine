20390f4c75ca42c8eba4fdf838c59f86e0d458ea
Sayak Paul <spsayakpaul@gmail.com>
2020-09-09 10:18:48 +0530

    removed unncessary prints

--- create_pr_comment.py
+++ create_pr_comment.py
@@ -13,7 +13,6 @@ list_pr_response = requests.get("https://api.github.com/repos/sayakpaul/wine/pul
 if list_pr_response.status_code == 200:
 	latest_pr = list_pr_response.json()[0]
 	pr_id = latest_pr["url"].split("/")[-1]
-	print(pr_id)
 
 # Get details about it
 single_pr_response = requests.get("https://api.github.com/repos/sayakpaul/wine/pulls/{}".format(pr_id),
@@ -26,7 +25,6 @@ single_pr_response = requests.get("https://api.github.com/repos/sayakpaul/wine/p
 if single_pr_response.status_code == 200:
 	description = single_pr_response.json()
 	issue_comment_url = description["_links"]["comments"]["href"]
-	print(issue_comment_url)
 
 # Load the run URL
 run_url = open("run.txt", "r").read()
@@ -42,4 +40,3 @@ comment_response = requests.post(issue_comment_url,
 	} ,
 	data=json.dumps(data)
 )
-print(comment_response.text)
