72b758a6c0866f47feec41ac890528c1fb5a75de
Daniel Klevebring <daniel.klevebring@gmail.com>
2020-12-22 15:40:30 +0100

    path fix

--- install-gcloud.sh
+++ install-gcloud.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+BASE=${PWD}
 sudo apt update
 mkdir -p gcp
 cd gcp
@@ -8,4 +9,4 @@ tar xvzf google-cloud-sdk-307.0.0-linux-x86_64.tar.gz
 
 cd google-cloud-sdk
 ./install.sh -q
-source /home/runner/gcp/google-cloud-sdk/path.bash.inc
+source ${BASE}/gcp/google-cloud-sdk/path.bash.inc
