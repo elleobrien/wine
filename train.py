42e0f8a8b458cce21fcee5d99d3139f6b62a5db2
Daniel Klevebring <daniel.klevebring@gmail.com>
2020-12-22 13:29:17 +0100

    move data

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ seed = 42
 ################################
 
 # Load in the data
-df = pd.read_csv("wine_quality.csv")
+df = pd.read_csv("data/wine_quality.csv")
 
 # Split into train and test sections
 y = df.pop("quality")
