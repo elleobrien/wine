46e017a4e3eeac1ca648e863cbe43c9cdebf7943
Daniel Klevebring <daniel.klevebring@gmail.com>
2020-12-26 21:35:46 +0100

    -q

--- install-gcloud.sh
+++ install-gcloud.sh
@@ -4,7 +4,7 @@ BASE=${PWD}
 sudo apt update
 mkdir -p gcp
 cd gcp
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-307.0.0-linux-x86_64.tar.gz >/dev/null
+wget -q https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-307.0.0-linux-x86_64.tar.gz
 tar xvzf google-cloud-sdk-307.0.0-linux-x86_64.tar.gz 2>&1 >/dev/null
 
 cd google-cloud-sdk
