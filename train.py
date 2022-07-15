51ce61c26a8d96f54c69dc7e2554e5b08118c5d9
thanvanquang <44353832+thanvanquang@users.noreply.github.com>
2022-07-15 14:09:08 +0700

    change max_depth=7

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random
 #################################
 
 # Fit a model on the train section
-regr = RandomForestRegressor(max_depth=5, random_state=seed)
+regr = RandomForestRegressor(max_depth=7, random_state=seed)
 regr.fit(X_train, y_train)
 
 # Report training set score
