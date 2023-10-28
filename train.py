35c3f31a43121654324119facd338fdc746ccc35
phunc20 <wucf20@gmail.com>
2023-10-28 11:49:52 +0700

    make train.py prettier and requirements.txt reproducible

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd 
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
+
 # Set random seed
 seed = 42
 
@@ -11,18 +12,18 @@ seed = 42
 ########## DATA PREP ###########
 ################################
 
-# Load in the data
+# Load the data
 df = pd.read_csv("wine_quality.csv")
 
-# Split into train and test sections
 y = df.pop("quality")
+# Split into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
 
 #################################
 ########## MODELLING ############
 #################################
 
-# Fit a model on the train section
+# Fit a model to the training set
 regr = RandomForestRegressor(max_depth=2, random_state=seed)
 regr.fit(X_train, y_train)
 
@@ -33,8 +34,8 @@ test_score = regr.score(X_test, y_test) * 100
 
 # Write scores to a file
 with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+    outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+    outfile.write("Test variance explained:     %2.1f%%\n" % test_score)
 
 
 ##########################################
@@ -43,17 +44,17 @@ with open("metrics.txt", 'w') as outfile:
 # Calculate feature importance in random forest
 importances = regr.feature_importances_
 labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
+feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature", "importance"])
 feature_df = feature_df.sort_values(by='importance', ascending=False,)
 
 # image formatting
-axis_fs = 18 #fontsize
-title_fs = 22 #fontsize
+axis_fs = 18  # fs for fontsize
+title_fs = 22
 sns.set(style="whitegrid")
 
 ax = sns.barplot(x="importance", y="feature", data=feature_df)
 ax.set_xlabel('Importance',fontsize = axis_fs) 
-ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
+ax.set_ylabel('Feature', fontsize = axis_fs)  # ylabel
 ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 
 plt.tight_layout()
@@ -67,7 +68,7 @@ plt.close()
 
 y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
 y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
+res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true", "pred"])
 
 ax = sns.scatterplot(x="true", y="pred",data=res_df)
 ax.set_aspect('equal')
@@ -75,11 +76,10 @@ ax.set_xlabel('True wine quality',fontsize = axis_fs)
 ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
 ax.set_title('Residuals', fontsize = title_fs)
 
-# Make it pretty- square aspect ratio
+# Make it pretty -- square aspect ratio
 ax.plot([1, 10], [1, 10], 'black', linewidth=1)
 plt.ylim((2.5,8.5))
 plt.xlim((2.5,8.5))
 
 plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
-
+plt.savefig("residuals.png", dpi=120)
