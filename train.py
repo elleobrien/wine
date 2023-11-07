e4747bfb2d326d2e50fde9bfacd708d80030a0e7
jfcalder <jfcalder@gmail.com>
2023-11-07 13:01:09 -0300

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ seed = 42
 df = pd.read_csv("wine_quality.csv")
 
 # Split into train and test sections
-y = df.pop("quality")
+y = df.pop("CALIDAD")
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
 
 #################################
